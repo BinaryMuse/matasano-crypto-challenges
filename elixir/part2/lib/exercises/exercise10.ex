a6f8345f35f34478d7bbbac285643085b2ac6a52
Brandon Tilley <brandon@brandontilley.com>
2013-07-05 21:13:16 -0700

    Add exercise 10

---
+++ elixir/part2/lib/exercises/exercise10.ex
@@ -0,0 +1,56 @@
+# 10. Implement CBC Mode
+#
+# In CBC mode, each ciphertext block is added to the next plaintext
+# block before the next call to the cipher core.
+#
+# The first plaintext block, which has no associated previous ciphertext
+# block, is added to a "fake 0th ciphertext block" called the IV.
+#
+# Implement CBC mode by hand by taking the ECB function you just wrote,
+# making it encrypt instead of decrypt (verify this by decrypting
+# whatever you encrypt to test), and using your XOR function from
+# previous exercise.
+#
+# DO NOT CHEAT AND USE OPENSSL TO DO CBC MODE, EVEN TO VERIFY YOUR
+# RESULTS. What's the point of even doing this stuff if you aren't going
+# to learn from it?
+#
+# The buffer at:
+# 
+#     https://gist.github.com/3132976
+#
+# is intelligible (somewhat) when CBC decrypted against "YELLOW
+# SUBMARINE" with an IV of all ASCII 0 (\x00\x00\x00 &c)
+
+defmodule Part2.Exercise10 do
+  use ExUnit.Case
+
+  alias Matasano.Bytes
+  alias Matasano.Plaintext
+  alias Matasano.Crypto.AES128
+
+  def run do
+    data = Matasano.Loader.text_from_file("fixtures/10.txt") |> Bytes.from_base64
+    null_iv = List.duplicate(0, 16) |> list_to_binary
+
+    # Test decryption visually
+    decrypted = AES128.cbc_decrypt(data, "YELLOW SUBMARINE", null_iv)
+    Enum.each binary_to_list(decrypted), fn(byte) ->
+      if Enum.member? Plaintext.printables, byte do
+        IO.write [byte]
+      else
+        IO.write "?"
+      end
+    end
+
+    # Test encryption by re-encrypting decrypted data
+    encrypted = AES128.cbc_encrypt(decrypted, "YELLOW SUBMARINE", null_iv)
+    assert encrypted == data
+  end
+end
+
+# $ mix exercise 10
+# I'm back and I'm ringin' the bell
+# A rockin' on the mike while the fly girls yell
+# In ecstasy in the back of me
+# Well that's my DJ Deshay cuttin' all them Z's
--- elixir/part2/lib/matasano/crypto/aes128.ex
+++ elixir/part2/lib/matasano/crypto/aes128.ex
@@ -5,21 +5,62 @@ defmodule Matasano.Crypto.AES128 do
   # CBC mode with a 0-filled IV on each block, one at a time.
   def ecb_encrypt(text, key) do
     parts = binary_to_list(text) |> Matasano.Bytes.split_into(16)
-    parts = Enum.map parts, encrypt_block(&1, key)
+    parts = Enum.map parts, ecb_encrypt_block(&1, key)
     Enum.join parts, ""
   end
 
   def ecb_decrypt(text, key) do
     parts = binary_to_list(text) |> Matasano.Bytes.split_into(16)
-    parts = Enum.map parts, decrypt_block(&1, key)
+    parts = Enum.map parts, ecb_decrypt_block(&1, key)
     Enum.join parts, ""
   end
 
-  def encrypt_block(part, key) do
+  def ecb_encrypt_block(part, key) when is_list(part) do
     :crypto.aes_cbc_128_encrypt(key, @ecb_iv, list_to_binary(part))
   end
 
-  def decrypt_block(part, key) do
+  def ecb_encrypt_block(part, key) do
+    :crypto.aes_cbc_128_encrypt(key, @ecb_iv, part)
+  end
+
+  def ecb_decrypt_block(part, key) when is_list(part) do
     :crypto.aes_cbc_128_decrypt(key, @ecb_iv, list_to_binary(part))
+  end
+
+  def ecb_decrypt_block(part, key) do
+    :crypto.aes_cbc_128_decrypt(key, @ecb_iv, part)
+  end
+
+  # Hand-rolled CBC mode, using our ECB mode above (which is,
+  # unfortunately, implemented with CBC mode).
+  def cbc_encrypt(bytes, key, iv) do
+    parts = bytes |> Matasano.Bytes.split_into(16)
+    cbc_encrypt_block(iv, parts, key, [])
+  end
+
+  def cbc_encrypt_block(_, [], _, acc), do: Enum.reverse(acc) |> list_to_binary
+
+  def cbc_encrypt_block(last_block, [this_block | tail], key, acc) do
+    xored_block = Matasano.Bytes.xor_sum(last_block, this_block)
+    encrypted = ecb_encrypt_block(xored_block, key)
+    cbc_encrypt_block(encrypted, tail, key, [encrypted | acc])
+  end
+
+  def cbc_decrypt(bytes, key, iv) do
+    parts = bytes |> Matasano.Bytes.split_into(16)
+    cbc_decrypt_block(iv, parts, key, [])
+  end
+
+  def cbc_decrypt_block(_, [], _, acc), do: Enum.reverse(acc) |> list_to_binary
+
+  def cbc_decrypt_block(last_block, [this_block | tail], key, acc) do
+    decrypted = ecb_decrypt_block(this_block, key)
+    xored_block = Matasano.Bytes.xor_sum(decrypted, last_block)
+    # The *unencrypted* block is sent as the "last_block" parameter
+    # to be XOR'd with the next block, while the xored_block
+    # becomes the next block of plaintext.
+    #
+    # See https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Cipher-block_chaining_.28CBC.29
+    cbc_decrypt_block(this_block, tail, key, [xored_block | acc])
   end
 end
